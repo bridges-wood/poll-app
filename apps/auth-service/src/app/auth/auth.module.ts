498dec8adeb210a15693e7414e24d84772a08fe1
Max Wood <bridges.wood@gmail.com>
2024-04-07 15:17:46 +0100

    Increase JWT token expiration time to 15 minutes

--- apps/auth-service/src/app/auth/auth.module.ts
+++ apps/auth-service/src/app/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { AuthService } from './auth.service';
       global: true,
       secret: process.env['JWT_SECRET'],
       signOptions: {
-        expiresIn: '5m',
+        expiresIn: '15m',
       },
     }),
   ],
