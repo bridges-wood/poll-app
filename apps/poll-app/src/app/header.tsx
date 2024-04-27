052e1fd9de48b85e60cc19b7b6c9f28d2e268c59
Max Wood <bridges.wood@gmail.com>
2024-04-27 22:04:44 +0100

    Update header component in poll-app

--- apps/poll-app/src/app/header.tsx
+++ apps/poll-app/src/app/header.tsx
@@ -1,16 +1,50 @@
 'use client';
 
+import { Button } from '@org/ui-kit/ui/button';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+} from '@org/ui-kit/ui/nav';
 import { ThemeToggle } from '@org/ui-kit/ui/theme-toggle';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const AppHeader = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <header className="w-full flex justify-between items-center h-20 px-4">
-      <Link href="/">
-        <h1 className="text-5xl font-extralight">Pollstr</h1>
-      </Link>
-      <ThemeToggle />
-    </header>
+    <Navbar shouldHideOnScroll onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent id="brand-group">
+        {/* <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="sm:hidden"
+        /> */}
+        <NavbarBrand>
+          <Link href="/">
+            <h1 className="text-5xl font-extralight">Pollstr</h1>
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent justify="end" id="end-group">
+        <NavbarItem>
+          <Button variant="outline" asChild>
+            <Link href="/login">Login</Link>
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <ThemeToggle />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        <NavbarMenuItem>
+          <Link href="/polls">Polls</Link>
+        </NavbarMenuItem>
+      </NavbarMenu>
+    </Navbar>
   );
 };
 
