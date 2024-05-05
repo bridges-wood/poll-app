3fd258aa6f011e58b9f3d10e0cacd38d41d10c55
Max Wood <bridges.wood@gmail.com>
2024-05-05 12:59:40 +0100

    Refactor providers.tsx to use @urql/exchange-auth and @urql/exchange-graphcache

--- apps/poll-app/src/components/providers.tsx
+++ apps/poll-app/src/components/providers.tsx
@@ -1,15 +1,51 @@
 'use client';
 
-import { ClientFactory } from '@poll-app/lib/api/client-factory';
-import { UrqlProvider } from '@urql/next';
+import { authExchange } from '@urql/exchange-auth';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import {
+  UrqlProvider,
+  createClient,
+  fetchExchange,
+  ssrExchange,
+} from '@urql/next';
 import { ThemeProvider } from 'next-themes';
+import { parseCookies } from 'nookies';
 import { FC, PropsWithChildren, useMemo } from 'react';
 
 const Providers: FC<PropsWithChildren> = ({ children }) => {
   const [client, ssr] = useMemo(() => {
-    const clientFactory = new ClientFactory({ isClientSide: true });
-    const client = clientFactory.getClient();
-    const ssr = clientFactory.getSsr();
+    const ssr = ssrExchange({ isClient: typeof window !== 'undefined' });
+
+    const client = createClient({
+      url:
+        process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:3000/graphql',
+      exchanges: [
+        cacheExchange({}),
+        authExchange(async (utils) => {
+          const { token } = parseCookies();
+
+          return {
+            addAuthToOperation: (operation) => {
+              if (!token) return operation;
+              return utils.appendHeaders(operation, {
+                Authorization: `Bearer ${token}`,
+              });
+            },
+            didAuthError: (error) => {
+              console.log(error.graphQLErrors.map((e) => e.message));
+              return error.graphQLErrors.some(
+                (e) => e.message === 'jwt expired',
+              );
+            },
+            async refreshAuth() {
+              console.log('Refreshing token');
+            },
+          };
+        }),
+        ssr,
+        fetchExchange,
+      ],
+    });
 
     return [client, ssr];
   }, []);
--- apps/poll-app/src/lib/api/registered-client.ts
+++ apps/poll-app/src/lib/api/registered-client.ts
@@ -1,8 +1,40 @@
+import { createClient, fetchExchange } from '@urql/core';
+import { authExchange } from '@urql/exchange-auth';
+import { cacheExchange } from '@urql/exchange-graphcache';
 import { registerUrql } from '@urql/next/rsc';
-import { ClientFactory } from './client-factory';
+import { cookies } from 'next/headers';
 
-const factory = new ClientFactory({ isClientSide: false });
+const { getClient } = registerUrql(() => {
+  return createClient({
+    exchanges: [
+      cacheExchange({}),
+      authExchange(async (utils) => {
+        const token = cookies().get('token')?.value;
 
-const { getClient } = registerUrql(factory.getClient.bind(factory));
+        return {
+          addAuthToOperation: (operation) => {
+            if (!token) return operation;
+            return utils.appendHeaders(operation, {
+              Authorization: `Bearer ${token}`,
+            });
+          },
+          didAuthError: (error) => {
+            console.log(error.graphQLErrors.map((e) => e.message));
+            return error.graphQLErrors.some((e) => e.message === 'jwt expired');
+          },
+          async refreshAuth() {
+            console.log('Refreshing token');
+          },
+        };
+      }),
+      fetchExchange,
+    ],
+    url:
+      process.env.NODE_ENV === 'production'
+        ? process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:3000/graphql'
+        : 'http://localhost:3000/graphql',
+    requestPolicy: 'cache-and-network',
+  });
+});
 
 export default getClient;
