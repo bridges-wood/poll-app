db46e910ad5b561eab116e3a6cdd70f4b1cc7c21
Max Wood <bridges.wood@gmail.com>
2024-05-05 19:01:15 +0100

    Update home layout

--- apps/poll-app/src/app/home/layout.tsx
+++ apps/poll-app/src/app/home/layout.tsx
@@ -9,15 +9,15 @@ const HomeLayout = ({
   feed,
 }: {
   children: React.ReactNode;
-  feed: never;
+  feed: React.ReactNode;
 }) => {
   return (
     <div className="h-[calc(100vh_-_64px)] 2xl:h-[calc(84vh_-_64px)]">
       <div className="grid h-full gap-5 md:grid-cols-12">
-        <Sidebar />
+        <Sidebar className="max-sm:hidden" />
         <main className="grid place-items-center md:col-span-9 md:h-screen">
           {feed}
-          {children}
+          {/* {children} */}
         </main>
       </div>
     </div>
--- apps/poll-app/src/app/home/sidebar.tsx
+++ apps/poll-app/src/app/home/sidebar.tsx
@@ -1,18 +1,49 @@
-import { Skeleton } from '@org/ui-kit/ui/skeleton';
-import ProfileIconContainer from '@poll-app/components/profile/profile-icon';
+import { Button } from '@org/ui-kit/ui/button';
+import {
+  AvatarIcon,
+  BellIcon,
+  DotsHorizontalIcon,
+  GlobeIcon,
+  HomeIcon,
+  Pencil2Icon,
+} from '@radix-ui/react-icons';
 import Link from 'next/link';
-import { Suspense } from 'react';
+import { FC } from 'react';
 
-const HomeSidebar = () => {
+interface HomeSidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const HomeSidebar: FC<HomeSidebarProps> = (props) => {
   return (
-    <aside className="md:col-span-2 md:h-screen bg-red-500">
-      <Suspense fallback={<Skeleton className="w-8 h-8 rounded-full" />}>
-        <Link href="/profile">
-          <ProfileIconContainer />
-        </Link>
-      </Suspense>
-      <div>HomeSidebar</div>
-    </aside>
+    <div {...props}>
+      <ul>
+        <li className="flex items-center gap-2">
+          <HomeIcon />
+          <Link href="/home">Home</Link>
+        </li>
+        <li className="flex items-center gap-2">
+          <GlobeIcon />
+          <Link href="/explore">Explore</Link>
+        </li>
+        <li className="flex items-center gap-2">
+          <BellIcon />
+          <Link href="/notifications">Notifications</Link>
+        </li>
+        <li className="flex items-center gap-2">
+          <AvatarIcon />
+          <Link href="/profile">Profile</Link>
+        </li>
+        <li className="flex items-center gap-2">
+          <DotsHorizontalIcon />
+          <Link href="/more">More</Link>
+        </li>
+        <li>
+          <Button variant="default" className="flex items-center gap-2">
+            <Pencil2Icon />
+            Post
+          </Button>
+        </li>
+      </ul>
+    </div>
   );
 };
 
