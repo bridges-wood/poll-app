232aa187a9f6af9035e16fcd188d43c05e3ae668
Max Wood <bridges.wood@gmail.com>
2025-04-27 09:38:23 +0100

    Refactor Index component to use async user authentication check and redirect for logged-in users

--- apps/poll-app/src/app/page.tsx
+++ apps/poll-app/src/app/page.tsx
@@ -1,7 +1,13 @@
 import { Button } from '@org/ui-kit/ui/button';
+import { getLoggedInUserId } from '@poll-app/utils/get-logged-in-user-id';
+import { isEmpty } from 'lodash';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
-const Index = () => {
+const Index = async () => {
+  const isLoggedIn = !isEmpty(await getLoggedInUserId());
+  if (isLoggedIn) return redirect('/home');
+
   return (
     <section className="grid h-[calc(100vh_-_64px)] place-items-center">
       <div className="flex flex-col gap-2">
